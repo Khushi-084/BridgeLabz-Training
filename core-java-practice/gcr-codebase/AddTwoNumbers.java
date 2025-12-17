0dd19705077afd71fefbdd39aa5ebe2650bc437c
Khushi-084 <122378361+Khushi-084@users.noreply.github.com>
2025-12-17 15:53:41 +0530

    Print the sum of two numbers

---
+++ core-java-practice/gcr-codebase/AddTwoNumbers.java
@@ -0,0 +1,22 @@
+ import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input from user
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        // Calculating sum
+        int sum = num1 + num2;
+
+        // Printing result
+        System.out.println("Sum = " + sum);
+
+    }
+}
+
