3f947d93241d32016e9de7cc436782635976267d
Khushi-084 <122378361+Khushi-084@users.noreply.github.com>
2025-12-17 22:42:21 +0530

    Check whether the number is prime or not

---
+++ core-java-practice/leet-code-codebase/PrimeNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Input number
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        boolean isPrime = true;
+
+        // Prime logic
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        // Output
+        if (isPrime) {
+            System.out.println(n + " is a Prime number.");
+        } else {
+            System.out.println(n + " is NOT a Prime number.");
+        }
+
+       
+    }
+}
