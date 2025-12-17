c6227bc8cef4734e6d7beb504a25586f3a36ff16
Khushi-084 <122378361+Khushi-084@users.noreply.github.com>
2025-12-17 15:43:29 +0530

    Check whether the parentheses are valid or not

---
+++ core-java-practice/leet-code-codebase/ValidParentheses.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+
+            // Push opening brackets
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } 
+            // Handle closing brackets
+            else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String str = "{[()]}";
+        System.out.println(isValid(str)); 
+    }
+}
