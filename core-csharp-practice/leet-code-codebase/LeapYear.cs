2676dc87c102c48c135af3be26f1e0ea7f3c19a5
Khushi-084 <122378361+Khushi-084@users.noreply.github.com>
2025-12-18 15:06:01 +0530

    To check whether the year is a leap year or not

---
+++ core-csharp-practice/leet-code-codebase/LeapYear.cs
@@ -0,0 +1,17 @@
+ class LeapYear {
+    public static void Main(String[] args) {
+
+       Console.Write("Enter a year:");
+       int year= Convert.To32(Console.ReadLine());
+
+        
+        
+        // Leap year logic
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            Console.WriteLine(year + " is a Leap Year.");
+        } else {
+            Console.WriteLine(year + " is NOT a Leap Year.");
+        }
+
+    }
+}
