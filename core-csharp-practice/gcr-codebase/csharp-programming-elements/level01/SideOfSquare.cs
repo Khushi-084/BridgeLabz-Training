2fbbb26427194e69bcf40af1671f2cea6bca6f60
Khushi-084 <122378361+Khushi-084@users.noreply.github.com>
2025-12-19 16:52:55 +0530

    To calculate the side of a square

---
+++ core-csharp-practice/gcr-codebase/csharp-programming-elements/level01/SideOfSquare.cs
@@ -0,0 +1,13 @@
+using System;
+public class SideOfSquare{
+  public static void Main(String [] args){
+
+    //taking input from user
+    Console.Write("Enter the perimeter: ");
+    int perimeter = int.Parse(Console.ReadLine());
+    //formulating the length of side
+    int side = p/4;
+    Console.WriteLine("The length of the side is " + side + " whose perimeter is " + p);
+
+  }
+}
