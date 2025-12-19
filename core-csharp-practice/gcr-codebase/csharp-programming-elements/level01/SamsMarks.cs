ae525386ed0ef95a893b2ec2f2f95d0299ba293c
Khushi-084 <122378361+Khushi-084@users.noreply.github.com>
2025-12-19 16:52:22 +0530

    To calculate the marks of sam

---
+++ core-csharp-practice/gcr-codebase/csharp-programming-elements/level01/SamsMarks.cs
@@ -0,0 +1,14 @@
+
+using System;
+class SamsMarks
+{
+  static void Main(string[] args){
+    int Maths=94;
+    int Physics=95;
+    int Chemistry=96;
+    //formula to calculate average
+    double average=(Maths+Physics+Chemistry)/3.0;
+    //output
+    Console.WriteLine("Sam's average mark in PCM is " + average);
+  }
+}
