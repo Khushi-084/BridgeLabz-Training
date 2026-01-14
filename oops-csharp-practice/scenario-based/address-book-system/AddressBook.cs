52f00bc504eeedb1ac4d404e8dfcaabfa87c45c6
Khushi-084 <122378361+Khushi-084@users.noreply.github.com>
2026-01-14 14:48:24 +0530

    Added the ability to add a new contact

--- oops-csharp-practice/scenario-based/address-book-system/AddressBook.cs
+++ oops-csharp-practice/scenario-based/address-book-system/AddressBook.cs
@@ -5,5 +5,18 @@ using System;
         public void DisplayWelcomeMessage(){
             Console.WriteLine("Welcome to Address Book Program");
         }
+        // UC-2: Add new contact
+        public void AddContact(Contact contact)
+        {
+            Console.WriteLine("\nContact Added Successfully!");
+            Console.WriteLine("Contact Details:");
+            Console.WriteLine($"Name: {contact.FirstName} {contact.LastName}");
+            Console.WriteLine($"Address: {contact.Address}");
+            Console.WriteLine($"City: {contact.City}");
+            Console.WriteLine($"State: {contact.State}");
+            Console.WriteLine($"Zip: {contact.Zip}");
+            Console.WriteLine($"Phone: {contact.PhoneNumber}");
+            Console.WriteLine($"Email: {contact.Email}");
+        }
     }
 
