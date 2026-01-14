9dc99ca7149f0ed428cfe6f7908d1e45e801382b
Khushi-084 <122378361+Khushi-084@users.noreply.github.com>
2026-01-14 12:46:27 +0530

    Added the ability to add a contact in address book with details

--- oops-csharp-practice/scenario-based/address-book-system/AddressBookMain.cs
+++ oops-csharp-practice/scenario-based/address-book-system/AddressBookMain.cs
@@ -1,7 +1,9 @@
 using System;
 // Main class for Address Book System
     class AddressBookMain{
+
         public static void Main(string[] args){
+            // Creating an instance of AddressBook and displaying welcome message
             IAddressBook addressBook = new AddressBook();
             addressBook.DisplayWelcomeMessage();
         }
