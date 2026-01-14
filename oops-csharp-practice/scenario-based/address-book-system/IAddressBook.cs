f19f7cefe0fdad991940cd0c5f47430da1f388fc
Khushi-084 <122378361+Khushi-084@users.noreply.github.com>
2026-01-14 14:59:10 +0530

    Added the ability to edit existing contactperson using their name

--- oops-csharp-practice/scenario-based/address-book-system/IAddressBook.cs
+++ oops-csharp-practice/scenario-based/address-book-system/IAddressBook.cs
@@ -4,5 +4,6 @@ using System;
         
         void DisplayWelcomeMessage();
         void AddContact(Contact contact);      // UC-2 added
+        void EditContact(string firstName);    // UC-3 added
     }
 
