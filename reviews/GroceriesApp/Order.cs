e05426edc6d237ab4da83b6db61a8c1cdb8ff0d3
Khushi-084 <122378361+Khushi-084@users.noreply.github.com>
2026-01-19 23:22:12 +0530

    Solved question in review on 07-01-2026

---
+++ reviews/GroceriesApp/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceriesApp.GroceriesApp
+{
+    internal class Order
+    {
+        //method to calculate item cost 
+        public int CalculateItemCost(int price, int quantity)
+        {
+            return price * quantity;
+        }
+        //method to get item by index
+        public bool IsValidIndex(int index, int length)
+        {
+            return index >= 0 && index < length;
+        }
+        //method to calculate discount if applicalbe
+        public int CalculateDiscount(int price)
+        {
+            return price * (20 / 100);
+        }
+    }    
+}
