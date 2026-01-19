37df32d76f292b5717c51d75398de1cd957c03f1
Khushi-084 <122378361+Khushi-084@users.noreply.github.com>
2026-01-19 23:22:54 +0530

    Solved question in review on 19-01-2026

---
+++ reviews/HospitalManagement/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Review.HospitalManagement
+{
+    internal abstract class Patient
+    {
+            public int PatientId { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public abstract double GetBill();
+            public abstract void DisplayInfo();
+        }
+    }
+
