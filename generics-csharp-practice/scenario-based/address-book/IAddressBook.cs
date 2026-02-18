a836b2260e54556d5b3af3cb09fe83fc26b288ea
Khushi-084 <122378361+Khushi-084@users.noreply.github.com>
2026-02-18 16:42:53 +0530

    Added functionality till UC-13 and exceptions

--- generics-csharp-practice/scenario-based/address-book/IAddressBook.cs
+++
@@ -1,8 +0,0 @@
-// interface for the address book
-public interface IAddressBook{
-    void DisplayWelcomeMessage();
-    void AddContact();
-    void EditContact();
-    void DeleteContact();
-    void AddMultipleContacts();
-}
